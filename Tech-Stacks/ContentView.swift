ad6417218090744e5aa917508452119a32fb2c82
Marc Boanas <marcjohnboanas@gmail.com>
2020-01-24 10:37:56 +0000

    Initial Commit

---
+++ Tech-Stacks/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Tech-Stacks
+//
+//  Created by Marc Boanas on 24/01/2020.
+//  Copyright © 2020 Marc Boanas. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
